0c0f9e48b0d6b13b4be91570a440acba24af725c
Jones <jnsiah@gsumail.gram.edu>
2026-05-21 18:50:23 -0500

    Connect FastAPI to local PostgreSQL database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://username:password@localhost/interview_platform"
+DATABASE_URL = "postgresql://jones@localhost/interview_platform"
 
 engine = create_engine(DATABASE_URL)
 
